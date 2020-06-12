d53372bdecf6e1db6be3b6ccb3d6144a5d66dac1
André Arnesson <andre.arnesson@gmail.com>
2020-06-12 23:54:00 +0200

    en början

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+<div className = 'firstText'>
+<h1 >Welcome to recipos!</h1>
+</div>
+    
+
+  );
+}
+
+export default App;
